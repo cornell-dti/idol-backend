da79ac4b6a693e0b24b53d27a19a21d901ab57bb
JBoss925 <jaggerbrulato@gmail.com>
2020-09-03 20:28:01 -0400

    print malformed private key

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import serverless from 'serverless-http';
-// import { db as database } from './firebase';
+import { db as database } from './firebase';
 
 const app = express();
 const router = express.Router();
-// const db = database;
+const db = database;
 const PORT = process.env.PORT || 9000;
 
 let numReq = 1;
