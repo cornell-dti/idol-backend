9eee8918cb63c8f49d72f7f1d1f1d86503767826
JBoss925 <jaggerbrulato@gmail.com>
2020-09-06 06:07:34 -0400

    fixed several bugs, added uuid system for teams (instead of names, which caused duplication on name changes)

--- src/teamAPI.ts
+++ src/teamAPI.ts
@@ -5,14 +5,17 @@ import { checkLoggedIn } from "./api";
 import { firestore } from "firebase-admin";
 import { materialize } from "./util";
 import { Member } from "./memberAPI";
+import { v4 as uuidv4 } from 'uuid';
 
 export type DBTeam = {
-  names: string,
+  uuid: string,
+  name: string,
   leaders: firestore.DocumentReference[],
   members: firestore.DocumentReference[]
 }
 
 export type Team = {
+  uuid: string,
   name: string,
   leaders: Member[],
   members: Member[]
@@ -34,10 +37,6 @@ export let setTeam = async (req, res) => {
     if (!canEdit) {
       res.status(200).json({ error: "User with email: " + req.session.email + " does not have permission to edit teams!" });
     } else {
-      if (!teamBody.name || teamBody.name === '') {
-        res.status(200).json({ error: "Couldn't edit team with undefined name!" });
-        return;
-      }
       if (teamBody.leaders.length > 0 && !teamBody.leaders[0].email) {
         res.status(200).json({ error: "Malformed leaders on POST!" });
         return;
@@ -47,7 +46,8 @@ export let setTeam = async (req, res) => {
         return;
       }
       let teamRef: DBTeam = {
-        names: teamBody.name,
+        uuid: teamBody.uuid ? teamBody.uuid : uuidv4(),
+        name: teamBody.name,
         leaders: teamBody.leaders.map(leader => db.doc('members/' + leader.email)),
         members: teamBody.members.map(mem => db.doc('members/' + mem.email))
       };
@@ -55,8 +55,8 @@ export let setTeam = async (req, res) => {
       if (existRes.findIndex(val => val === false) != -1) {
         res.status(200).json({ error: "Couldn't create team from members that don't exist!" });
       } else {
-        db.doc('teams/' + teamBody.name).set(teamRef).then(() => {
-          res.status(200).json({ status: "Success", team: teamBody });
+        db.doc('teams/' + teamRef.uuid).set(teamRef).then(() => {
+          res.status(200).json({ status: "Success", team: { ...teamBody, uuid: teamRef.uuid } });
         }).catch((reason) => {
           res.status(200).json({ error: "Couldn't edit team for reason: " + reason });
         });
@@ -68,20 +68,20 @@ export let setTeam = async (req, res) => {
 export let deleteTeam = async (req, res) => {
   if (checkLoggedIn(req, res)) {
     let teamBody = req.body as Team;
+    if (!teamBody.uuid || teamBody.uuid === '') {
+      res.status(200).json({ error: "Couldn't delete team with undefined uuid!" });
+      return;
+    }
     let member = await (await db.doc('members/' + req.session.email).get()).data();
-    let teamSnap = await (await db.doc('teams/' + teamBody.name).get());
+    let teamSnap = await (await db.doc('teams/' + teamBody.uuid).get());
     if (!teamSnap.exists) {
-      res.status(200).json({ error: "No team with name: " + teamBody.name });
+      res.status(200).json({ error: "No team with uuid: " + teamBody.uuid });
     } else {
       let canEdit = PermissionsManager.canEditTeams(member.role);
       if (!canEdit) {
         res.status(200).json({ error: "User with email: " + req.session.email + " does not have permission to delete teams!" });
       } else {
-        if (!teamBody.name || teamBody.name === '') {
-          res.status(200).json({ error: "Couldn't delete team with undefined name!" });
-          return;
-        }
-        db.doc('teams/' + teamBody.name).delete().then(() => {
+        db.doc('teams/' + teamBody.uuid).delete().then(() => {
           res.status(200).json({ status: "Success", team: teamBody });
         }).catch((reason) => {
           res.status(200).json({ error: "Couldn't delete team for reason: " + reason });
