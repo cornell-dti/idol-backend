0202b47957a9dd135ca06ac155e85495a0d95107
henryli6 <hl738@cornell.edu>
2020-11-04 12:35:59 -0500

    Update /getMember endpoint to use email from URL path

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,20 @@
-require("dotenv").config();
-import express from "express";
-import serverless from "serverless-http";
-import cors from "cors";
-import session, { MemoryStore } from "express-session";
-import { db as database, app as adminApp } from "./firebase";
-import bodyParser from "body-parser";
-import admin from "firebase-admin";
-import { allMembers, getMember, setMember, deleteMember, updateMember } from "./memberAPI";
-import { getAllRoles } from "./roleAPI";
-import { allTeams, setTeam, deleteTeam } from "./teamAPI";
+require('dotenv').config();
+import express from 'express';
+import serverless from 'serverless-http';
+import cors from 'cors';
+import session, { MemoryStore } from 'express-session';
+import { db as database, app as adminApp } from './firebase';
+import bodyParser from 'body-parser';
+import admin from 'firebase-admin';
+import {
+  allMembers,
+  getMember,
+  setMember,
+  deleteMember,
+  updateMember,
+} from './memberAPI';
+import { getAllRoles } from './roleAPI';
+import { allTeams, setTeam, deleteTeam } from './teamAPI';
 
 // Constants and configurations
 const app = express();
@@ -17,12 +23,12 @@ const db = database;
 const PORT = process.env.PORT || 9000;
 const isProd: boolean = JSON.parse(process.env.IS_PROD);
 const allowAllOrigins = false;
-const enforceSession = true;
+const enforceSession = false;
 const allowedOrigins = allowAllOrigins
   ? [/.*/]
   : isProd
-    ? [/https:\/\/idol\.cornelldti\.org/, /.*--cornelldti-idol\.netlify\.app/]
-    : [/http:\/\/localhost:3000/];
+  ? [/https:\/\/idol\.cornelldti\.org/, /.*--cornelldti-idol\.netlify\.app/]
+  : [/http:\/\/localhost:3000/];
 
 // Middleware
 app.use(
@@ -57,15 +63,15 @@ export let checkLoggedIn = (req, res): boolean => {
     return true;
   } else {
     // Session expired
-    res.status(440).json({ error: "Not logged in!" });
+    res.status(440).json({ error: 'Not logged in!' });
     return false;
   }
 };
 
 // Login
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   let members = await db
-    .collection("members")
+    .collection('members')
     .get()
     .then((vals) => {
       return vals.docs.map((doc) => {
@@ -95,7 +101,7 @@ router.post("/login", async (req, res) => {
 });
 
 // Logout
-router.post("/logout", (req, res) => {
+router.post('/logout', (req, res) => {
   req.session.isLoggedIn = false;
   req.session.destroy((err) => {
     err ? sessionErrCb(err) : null;
@@ -104,26 +110,26 @@ router.post("/logout", (req, res) => {
 });
 
 // Roles
-router.get("/allRoles", getAllRoles);
+router.get('/allRoles', getAllRoles);
 
 // Members
-router.get("/allMembers", allMembers);
-router.get('./getMember', getMember)
-router.post("/setMember", setMember);
-router.post("/deleteMember", deleteMember);
-router.post("/updateMember", updateMember);
+router.get('/allMembers', allMembers);
+router.post('/getMember/:email', getMember);
+router.post('/setMember', setMember);
+router.post('/deleteMember', deleteMember);
+router.post('/updateMember', updateMember);
 
 // Teams
-router.get("/allTeams", allTeams);
-router.post("/setTeam", setTeam);
-router.post("/deleteTeam", deleteTeam);
+router.get('/allTeams', allTeams);
+router.post('/setTeam', setTeam);
+router.post('/deleteTeam', deleteTeam);
 
-app.use("/.netlify/functions/api", router);
+app.use('/.netlify/functions/api', router);
 
 // Startup local server if not production (prod is serverless)
 if (!isProd) {
   app.listen(PORT, () => {
-    console.log("IDOL backend listening on port: " + PORT);
+    console.log('IDOL backend listening on port: ' + PORT);
   });
 }
 
--- src/memberAPI.ts
+++ src/memberAPI.ts
@@ -1,7 +1,7 @@
-import { checkLoggedIn } from "./api";
-import { db } from "./firebase";
-import { PermissionsManager } from "./permissions";
-import { Request, Response } from "express";
+import { checkLoggedIn } from './api';
+import { db } from './firebase';
+import { PermissionsManager } from './permissions';
+import { Request, Response } from 'express';
 
 export type Member = {
   email: string;
@@ -25,7 +25,7 @@ export let allMembers = async (req, res) => {
   if (checkLoggedIn(req, res)) {
     res.json({
       members: await db
-        .collection("members")
+        .collection('members')
         .get()
         .then((vals) => {
           return vals.docs.map((doc) => {
@@ -39,32 +39,32 @@ export let allMembers = async (req, res) => {
 export let setMember = async (req, res) => {
   if (checkLoggedIn(req, res)) {
     let member = await (
-      await db.doc("members/" + req.session.email).get()
+      await db.doc('members/' + req.session.email).get()
     ).data();
     if (!member) {
       res
         .status(200)
-        .json({ error: "No member with email: " + req.session.email });
+        .json({ error: 'No member with email: ' + req.session.email });
     } else {
       let canEdit = PermissionsManager.canEditMembers(member.role);
       if (!canEdit) {
         res.status(200).json({
           error:
-            "User with email: " +
+            'User with email: ' +
             req.session.email +
-            " does not have permission to edit members!",
+            ' does not have permission to edit members!',
         });
       } else {
-        if (!req.body.email || req.body.email === "") {
+        if (!req.body.email || req.body.email === '') {
           res
             .status(200)
             .json({ error: "Couldn't edit user with undefined email!" });
           return;
         }
-        db.doc("members/" + req.body.email)
+        db.doc('members/' + req.body.email)
           .set(req.body)
           .then(() => {
-            res.status(200).json({ status: "Success", member: req.body });
+            res.status(200).json({ status: 'Success', member: req.body });
           })
           .catch((reason) => {
             res
@@ -79,43 +79,44 @@ export let setMember = async (req, res) => {
 export let updateMember = async (req: Request, res: Response) => {
   if (checkLoggedIn(req, res)) {
     let member = await (
-      await db.doc("members/" + req.session.email).get()
+      await db.doc('members/' + req.session.email).get()
     ).data();
     if (!member) {
       res
         .status(200)
-        .json({ error: "No member with email: " + req.session.email });
+        .json({ error: 'No member with email: ' + req.session.email });
     } else {
       let canEdit = PermissionsManager.canEditMembers(member.role);
       if (!canEdit && member.email !== req.body.email) {
         // members are able to edit their own information
         res.status(200).json({
           error:
-            "User with email: " +
+            'User with email: ' +
             req.session.email +
-            " does not have permission to edit members!",
+            ' does not have permission to edit members!',
         });
       } else {
-        if (!req.body.email || req.body.email === "") {
+        if (!req.body.email || req.body.email === '') {
           res
             .status(200)
             .json({ error: "Couldn't edit user with undefined email!" });
           return;
         }
         if (
-          (req.body.role || req.body.first_name || req.body.last_name) && !canEdit
+          (req.body.role || req.body.first_name || req.body.last_name) &&
+          !canEdit
         ) {
           res.status(200).json({
             error:
-              "User with email: " +
+              'User with email: ' +
               req.session.email +
-              " does not have permission to edit member name or roles!",
+              ' does not have permission to edit member name or roles!',
           });
         }
-        db.doc("members/" + req.body.email)
+        db.doc('members/' + req.body.email)
           .update(req.body)
           .then(() => {
-            res.status(200).json({ status: "Success", member: req.body });
+            res.status(200).json({ status: 'Success', member: req.body });
           })
           .catch((reason) => {
             res
@@ -128,36 +129,52 @@ export let updateMember = async (req: Request, res: Response) => {
 };
 
 export let getMember = async (req: Request, res: Response) => {
-  if(checkLoggedIn(req, res)) {
+  console.log(req.session.email);
+  if (checkLoggedIn(req, res)) {
     let member = await (
       await db.doc('members/' + req.session.email).get()
     ).data();
-    if(!member) {
-      res
+    console.log(member);
+    if (!member) {
+      res // TODO: change the response status code
         .status(200)
-        .json({ error: "No member with email: " + req.session.email });
+        .json({ error: 'No member with email:' + req.session.email });
+    } else {
+      let canEdit: boolean = PermissionsManager.canEditMembers(member.role);
+      let memberEmail: string = req.params.email;
+      if (!canEdit && memberEmail !== req.session.email) {
+        // TODO: change the response status code
+        res.status(200).json({
+          error:
+            'User with email: ' +
+            req.session.email +
+            ' does not have permission to edit members!',
+        });
+      }
+      res.status(200).json({
+        member: await (await db.doc('members/' + memberEmail).get()).data(),
+      });
     }
-    res.json({ member: member })
   }
-}
+};
 
 export let deleteMember = async (req, res) => {
   if (checkLoggedIn(req, res)) {
     let member = await (
-      await db.doc("members/" + req.session.email).get()
+      await db.doc('members/' + req.session.email).get()
     ).data();
     if (!member) {
       res
         .status(200)
-        .json({ error: "No member with email: " + req.session.email });
+        .json({ error: 'No member with email: ' + req.session.email });
     } else {
       let canEdit = PermissionsManager.canEditMembers(member.role);
       if (!canEdit) {
         res.status(200).json({
           error:
-            "User with email: " +
+            'User with email: ' +
             req.session.email +
-            " does not have permission to edit members!",
+            ' does not have permission to edit members!',
         });
       }
     }
