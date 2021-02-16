aea8f654ea3338a30a5b76bc907074b73091409d
Henry Li <hl738@cornell.edu>
2021-02-16 14:21:54 -0500

    remove var

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
-import admin from "firebase-admin";
-var serviceAccount = require("../resources/idol-b6c68-firebase-adminsdk-h4e6t-40e4bd5536.json");
+import admin from 'firebase-admin';
+const serviceAccount = require('../resources/idol-b6c68-firebase-adminsdk-h4e6t-40e4bd5536.json');
 
 let configureAccount = (sa) => {
   let configAcc = sa;
@@ -9,14 +9,14 @@ let configureAccount = (sa) => {
   } catch (err) {
     parsedPK = process.env.FIREBASE_PRIVATE_KEY;
   }
-  configAcc["private_key"] = parsedPK;
-  configAcc["private_key_id"] = process.env.FIREBASE_PRIVATE_KEY_ID;
+  configAcc['private_key'] = parsedPK;
+  configAcc['private_key_id'] = process.env.FIREBASE_PRIVATE_KEY_ID;
   return configAcc;
 };
 
 export const app = admin.initializeApp({
   credential: admin.credential.cert(configureAccount(serviceAccount)),
-  databaseURL: "https://idol-b6c68.firebaseio.com",
+  databaseURL: 'https://idol-b6c68.firebaseio.com',
 });
 
 export const db = admin.firestore();
